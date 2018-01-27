59e9895dbe5ee623e1be8a17eb1fd378b56c4116
Deepanshu Utkarsh <deepanshu.utkarsh@tufts.edu>
2018-01-27 18:04:32 -0500

    add models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'end_time', 'location', 'host',)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio',)
+
+admin.site.register(models.Event, EventAdmin)
+admin.site.register(models.Profile, ProfileAdmin)
