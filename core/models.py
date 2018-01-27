bebe046ec77d5520a36e93ba580661c72ba2b20d
Deepanshu Utkarsh <deepanshu.utkarsh@tufts.edu>
2018-01-27 18:04:32 -0500

    add models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
-from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+
+class Event(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.PointField(srid=4269)
+    description = models.TextField()
+    host = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='events')
+    guests = models.ManyToManyField(Profile, related_name='interesting_events', blank=True)
---
+++ core/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'api/events', views.EventViewSet, 'Event')
+router.register(r'api/profiles', views.ProfileViewSet, 'Profile')
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^', include(router.urls)),
+]
