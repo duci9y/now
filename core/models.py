665e2cd432e56d1df944dce14baf1fae133c969d
Deepanshu Utkarsh <deepanshu.utkarsh@tufts.edu>
2018-01-28 04:04:00 -0500

    oh no honey

--- core/models.py
+++ core/models.py
@@ -2,8 +2,11 @@ from django.contrib.auth.models import User
 from django.contrib.gis.db import models
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True)
+    first_name = models.CharField(max_length=100, blank=True)
+    last_name = models.CharField(max_length=100, blank=True)
+
 
 class Event(models.Model):
     start_time = models.DateTimeField()
